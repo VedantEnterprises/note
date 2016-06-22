9a9d69c9ee52776e19b13c01ed3503e64d4f1d68
hanks-zyh <zhangyuhan2014@gmail.com>
2016-06-22 23:27:11 +0800

    add recycler dependence create NoteAdapter && NoteViewHolder

--- app/src/main/java/xyz/hanks/note/ui/activity/MainActivity.java
+++ app/src/main/java/xyz/hanks/note/ui/activity/MainActivity.java
@@ -2,14 +2,22 @@ package xyz.hanks.note.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import xyz.hanks.note.R;
+import xyz.hanks.note.ui.adapter.NoteAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new NoteAdapter());
     }
 }
---
+++ app/src/main/java/xyz/hanks/note/ui/adapter/NoteAdapter.java
@@ -0,0 +1,49 @@
+package xyz.hanks.note.ui.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import xyz.hanks.note.R;
+
+/**
+ * Created by hanks on 16/6/22.
+ */
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteItemViewHolder> {
+    @Override public NoteItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_note, parent, false);
+        return new NoteItemViewHolder(view);
+    }
+
+    @Override public void onBindViewHolder(NoteItemViewHolder holder, int position) {
+        holder.cbFavorite.setChecked(true);
+        holder.ivImage.setVisibility(View.VISIBLE);
+        holder.tvModifyTime.setText("昨天 12:45");
+        holder.tvNoteDetail.setText("点击进来看看还有什么惊喜");
+    }
+
+    @Override public int getItemCount() {
+        return 10;
+    }
+
+    class NoteItemViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView tvModifyTime;
+        public TextView tvNoteDetail;
+        public CheckBox cbFavorite;
+        public ImageView ivImage;
+
+        public NoteItemViewHolder(View itemView) {
+            super(itemView);
+            tvModifyTime = (TextView) itemView.findViewById(R.id.tv_modify_time);
+            tvNoteDetail = (TextView) itemView.findViewById(R.id.tv_note_detail);
+            cbFavorite = (CheckBox) itemView.findViewById(R.id.cb_favorite);
+            ivImage = (ImageView) itemView.findViewById(R.id.iv_img);
+        }
+
+    }
+}
