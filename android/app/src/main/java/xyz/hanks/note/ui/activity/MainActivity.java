f5817878a8d956d7e562a76041d665c34392e5d4
hanks-zyh <zhangyuhan2014@gmail.com>
2016-06-26 22:40:55 +0800

    set theme

--- android/app/src/main/java/xyz/hanks/note/ui/activity/MainActivity.java
+++ android/app/src/main/java/xyz/hanks/note/ui/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import xyz.hanks.note.R;
 import xyz.hanks.note.ui.adapter.NoteAdapter;
@@ -16,8 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new NoteAdapter());
+
     }
 }
