7daca3c0ae6439a1836be6e49f88ebefc9a797ae
hanks-zyh <zhangyuhan2014@gmail.com>
2016-06-27 23:35:14 +0800

    change listview to show EditActivity

--- android/app/src/main/java/xyz/hanks/note/ui/activity/EditActivity.java
+++ android/app/src/main/java/xyz/hanks/note/ui/activity/EditActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import xyz.hanks.note.R;
 
@@ -14,6 +18,16 @@ import xyz.hanks.note.R;
  * Created by hanks on 16/6/26.
  */
 public class EditActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    private String noteContent = "进来看看还有什么惊喜^_^\n" +
+            "\n" +
+            "我们支持把便签的文字直接发送到新<image w=858 h=483 describe= name=Note_123.jpg>浪微博，\n" +
+            "同时你再也不用忍受新浪的数字限制了，当文字超过 140 之后，便签会自动生成排版优雅、字体精美的图片长微博，希望我们的便签能够让你重新喜欢上不那么碎片的表达。试试点击右上角的小飞机，再点击随后出现的菜单中的 “以图片分享” 将图片分享至你的其他应用。\n" +
+            "\n" +
+            "便签内容现在支持分享至新浪长微博。";
+    private MyAdapter adapter;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, EditActivity.class);
@@ -32,5 +46,39 @@ public class EditActivity extends AppCompatActivity {
         toolbar.setTitle("编辑");
         toolbar.setNavigationIcon(R.drawable.abc_ic_ab_back_mtrl_am_alpha);
         setSupportActionBar(toolbar);
+
+
+        listView = (ListView) findViewById(R.id.listView);
+        adapter = new MyAdapter();
+        listView.setAdapter(adapter);
+
+
+        calc();
     }
+
+    private void calc() {
+
+    }
+
+
+    class MyAdapter extends BaseAdapter{
+
+        @Override public int getCount() {
+            return 0;
+        }
+
+        @Override public Object getItem(int position) {
+            return null;
+        }
+
+        @Override public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
+
+
 }
