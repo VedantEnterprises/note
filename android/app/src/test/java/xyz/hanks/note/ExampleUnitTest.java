03a6de0015adeadb32576f02726151e1138b1ceb
hanks-zyh <zhangyuhan2014@gmail.com>
2016-07-07 23:52:19 +0800

    test calc text width

--- android/app/src/test/java/xyz/hanks/note/ExampleUnitTest.java
+++ android/app/src/test/java/xyz/hanks/note/ExampleUnitTest.java
@@ -12,6 +12,20 @@ public class ExampleUnitTest {
         String s = "dasdasd\n";
         System.out.println("s = " + s.trim());
 
+        String s0 = "计算的一些规则";
+        String s1 = "今天是晴天今天是晴天今天是晴天今天是晴天";
+        String s2 = "一二三四五六七八九十一二三四五六七八九十";
+        String s3 = "abcdefghijklmnopqrstuvwxyzabcdefghijklmn";
+        String s4 = "，，，，，。。。。。，，，，，。。。。。";
+        String s5 = ".....,,,,,.....,,,,,.....,,,,,.....,,,,,.....,,,,,.....,,,,,.....,,,,,.....,,,,,....";
+        String s6 = "http://www.baidu.comhttp://www.baidu.co";
 
+        System.out.println("s0 = " + s0.length());
+        System.out.println("s1 = " + s1.length());
+        System.out.println("s2 = " + s2.length());
+        System.out.println("s3 = " + s3.length());
+        System.out.println("s4 = " + s4.length());
+        System.out.println("s5 = " + s5.length());
+        System.out.println("s6 = " + s6.length());
     }
 }
